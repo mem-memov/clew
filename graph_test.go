d4bdf3a144088f51000a8b38e8a01bf2ef91f4dc
Aleksandr Novikov <mem-memov@yandex.ru>
2021-08-08 20:13:50 +0300

    test

--- graph_test.go
+++ graph_test.go
@@ -8,27 +8,47 @@ import (
 func TestGraph(t *testing.T) {
 	g := NewGraph()
 
+	// a
+
 	want, a := uint(1), g.Create()
 	if a != want {
 		t.Errorf("want %v, got %v", want, a)
 	}
+
+	// a b
 
 	want, b := uint(2), g.Create()
 	if b != want {
 		t.Errorf("want %v, got %v", want, b)
 	}
 
+	// a b a->b
+
 	g.Update(a, b)
+
+	// a b a->b c
 
 	want, c := uint(4), g.Create()
 	if c != want {
 		t.Errorf("want %v, got %v", want, c)
 	}
 
+	// a b a->b c a->c
+
 	g.Update(a, c)
 
 	wantHeads, aHeads := []uint{2, 4}, g.Read(a)
 	if !reflect.DeepEqual(wantHeads, aHeads) {
 		t.Errorf("want %v, got %v", wantHeads, aHeads)
+	}
+
+	wantHeads, bHeads := []uint{}, g.Read(b)
+	if !reflect.DeepEqual(wantHeads, bHeads) {
+		t.Errorf("want %v, got %v", wantHeads, bHeads)
+	}
+
+	wantHeads, cHeads := []uint{}, g.Read(c)
+	if !reflect.DeepEqual(wantHeads, cHeads) {
+		t.Errorf("want %v, got %v", wantHeads, cHeads)
 	}
 }
